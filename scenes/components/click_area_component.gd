caf56d102e6f770a5b9f5c5c8319c4df60273edd
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-17 19:10:19 +0800

    feat: normalized click_area_component and added title screen

--- scenes/components/click_area_component.gd
+++ scenes/components/click_area_component.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
+signal input_detected
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("yeah it happened")
+		input_detected.emit()
---
+++ scenes/main.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_sex_input_detected():
+	print("fuck it we balling")
