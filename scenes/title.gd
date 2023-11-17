9d967726b299faea15030ee7bacc5ae3bccccbb5
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-17 19:30:13 +0800

    feat: added custom title screen buttons

--- scenes/title.gd
+++ scenes/title.gd
@@ -1,1 +1,14 @@
 extends Control
+
+
+func _on_new_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_continue_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	accept_event()
+	get_tree().quit()
