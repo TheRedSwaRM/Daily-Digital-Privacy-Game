7d6e92faf8437ea87846b0e130d8bc2dfdb58885
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-19 23:20:28 +0800

    fix #5 : fixed weird anti-aliasing for response box.

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
-	anim_player.play("intro")
-	await anim_player.animation_finished 
+	if !OS.is_debug_build():
+		anim_player.play("intro")
+		await anim_player.animation_finished 
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
