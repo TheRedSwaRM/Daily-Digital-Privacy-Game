d3c75e03d14afbc32491a1455a5122d04395b8aa
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-20 02:13:48 +0800

    chore: refined intro cutscene

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
-	if !OS.is_debug_build():
+	if OS.is_debug_build():
 		anim_player.play("intro")
 		await anim_player.animation_finished 
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
