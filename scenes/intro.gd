81958fa22c30c13d776639de377ca4a8e5487ea0
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-20 02:39:24 +0800

    chore: added skip intro option in intro cutscene

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -1,9 +1,10 @@
 extends Control
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@export var skip_intro: bool
 
 func _ready():
-	if OS.is_debug_build():
+	if skip_intro:
 		anim_player.play("intro")
 		await anim_player.animation_finished 
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
