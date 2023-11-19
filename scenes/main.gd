fd526a9859fc552d54a670d504909b207dd69c4c
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-19 23:27:46 +0800

    feat: added game controller

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,8 +1,1 @@
-extends Node2D
-
-func _ready():
-	print("we good")
-
-func _on_sex_input_detected():
-	DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/sample.dialogue"),"start")
-	
+extends Node
