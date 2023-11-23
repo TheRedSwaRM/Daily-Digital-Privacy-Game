2522691cc1a533f7be4b62a75a934a9bcd28fa49
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-11-23 22:53:12 +0800

    fix: decoupled main controller dependencies on ClickableArea 2

--- scenes/bedroom.gd
+++ scenes/bedroom.gd
@@ -1,11 +1,4 @@
 extends Node2D
 
-@onready var main_controller = get_parent()
-
 func _ready():
 	print("we good")
-
-func _on_sex_input_detected():
-	Events.change_map.emit("res://scenes/intro.tscn")
-	#DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/sample.dialogue"),"start")
-	
--- scenes/components/click_area_component.gd
+++ scenes/components/click_area_component.gd
@@ -9,20 +9,30 @@ class_name ClickableArea
 @export var collision_shape: CollisionPolygon2D
 
 ## The PackedScene to switch to in the case of pressing this area.
+## Note: Only have this WHEN you are sure that the following clickable area is 
+## for transferring only. Otherwise, use input_detected.
 @export var transfer_area: PackedScene
 
 @onready var label_node: Label = $Label
 
+## Use in place of in place of `input_event` connection in order to lessen
+## overhead on certain things. Like `input_event` args.
 signal input_detected
 
 func _ready():
 	label_node.text = label_name
 	label_node.position = collision_shape.position
 	
+# You might think: isn't think a bit repetitive? Ask yourself: isn't it a bit
+# too repetitive to just bust over the same function with the same arguments
+# on the map no less? When you can just serialize this thing in a node? Yeah,
+# I had my epiphany on that. IT JUST works.
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		input_detected.emit()
 
+		if transfer_area != null:
+			Events.change_map.emit(transfer_area.resource_path)
 
 func _on_mouse_entered():
 	label_node.visible = true
