8871a655812cd36a243a5f92a9a632d5d1e58786
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-12-08 16:04:06 +0800

    fix: location only shows if specified as premade

--- scenes/device/social_media_post.gd
+++ scenes/device/social_media_post.gd
@@ -3,8 +3,11 @@ extends PanelContainer
 
 @export var user_name: String
 @export var post_image: Texture2D
+## ONLY FOR PRE-MADE POSTS!
+@export var show_location_post: bool
 @export_multiline var location: String
 @export_multiline var post_text: String
+
 
 @onready var user_name_button = $VBoxContainer/Profile
 @onready var image_node = $VBoxContainer/PostContainer/PostImage
@@ -17,8 +20,10 @@ func _ready():
 	user_name_button.text = user_name
 	image_node.texture = post_image
 	if not Engine.is_editor_hint():
-		if Events.location:
+		if show_location_post:
 			location_label.text = location
+		else:
+			location_label.text = ""
 	post_text_node.text = post_text
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,5 +31,8 @@ func _process(delta):
 	if Engine.is_editor_hint():
 		user_name_button.text = user_name
 		image_node.texture = post_image
-		location_label.text = location
+		if show_location_post:
+			location_label.text = location
+		else:
+			location_label.text = ""
 		post_text_node.text = post_text
