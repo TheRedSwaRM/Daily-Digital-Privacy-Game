d89571baf2ed1358f2262b9fed35a281d417ef25
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-12-08 16:43:44 +0800

    feat: added scrolling for social media

--- scenes/device/social_media.gd
+++ scenes/device/social_media.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var home_feed = $HomeFeed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_home_feed_gui_input(event):
+	
+	# For scrolling purposes.
+	if event is InputEventMouseMotion and event.button_mask > 0:
+		home_feed.scroll_vertical = home_feed.scroll_vertical - event.relative.y
