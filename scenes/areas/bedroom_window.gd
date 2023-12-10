40cdee967915a04c172db468c7a462c852dff177
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-12-10 23:23:38 +0800

    fix: hide clock on nighttime

--- scenes/areas/bedroom_window.gd
+++ scenes/areas/bedroom_window.gd
@@ -3,10 +3,12 @@ extends Node2D
 @onready var cloud_animation = $AnimationPlayer
 @onready var background = $Background/RoomBackground
 @onready var clouds = $Background/Clouds
+@onready var clock = $Clock
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Events.check_game_switch("night"):
 		clouds.hide()
+		clock.hide()
 		background.texture = load("res://assets/images/bg/room3_night2.png")
 	else:
 		cloud_animation.play("clouds")
