85b327901fecae1a15621a2bfee8364d1bd8332b
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-02-02 12:00:03 +0800

    feat: working direct messages list

--- scenes/device/message_direct.gd
+++ scenes/device/message_direct.gd
@@ -1,12 +1,31 @@
-@tool
 extends Control
 
+@export var sender: String
+
+@onready var user_name = $MessageHeader/MessageUser
+@onready var message_list = $MessageScroller/MessageList
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	user_name.text = sender
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Engine.is_editor_hint():
+		user_name.text = sender
+		
+
+## Adds new post under MessageScroller
+func add_new_post(text_message: String):
+	var pre_text = preload("res://scenes/device/message_text_box.tscn")
+	var new_text = pre_text.instantiate()
+	
+	new_text.post_text = text_message
+	
+	message_list.add_child(new_text)
+
+
+
+func _on_debug_button_pressed():
+	add_new_post("gaysex")
---
+++ scenes/device/message_text_box.gd
@@ -0,0 +1,14 @@
+extends PanelContainer
+
+@export_multiline var post_text: String
+
+@onready var text_messsage = $TextMessage
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	text_messsage.text = post_text
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
