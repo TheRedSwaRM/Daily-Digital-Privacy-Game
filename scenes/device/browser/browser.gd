9e6af699f76fd57aa1f1a197eb58e0f82c688499
FlamingHerb <mbomisol@up.edu.ph>
2024-02-06 12:37:17 +0800

    feat: working browser tabs

--- scenes/device/browser/browser.gd
+++ scenes/device/browser/browser.gd
@@ -2,10 +2,28 @@ extends Control
 
 @onready var tabs_button = $TabsButton
 @onready var link_label = $LinkLabel
+@onready var tab_section_panel = $TabSection
+@onready var tab_section = $TabSection/Scroller/ScrollerHorizon
+@onready var available_tabs = $Tabs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Events.back_button_pressed.connect(_phone_back_button_pressed)
+	_create_tab_thumbnails()
+
+## Crazy helper function that will help us create the tab thumbnails which
+## will also trigger specific signals when needed.
+func _create_tab_thumbnails():
+	for child in available_tabs.get_children():
+		var pre_tab_sections = preload("res://scenes/device/browser/tab_thumbnail.tscn")
+		var new_tab_section = pre_tab_sections.instantiate()
+		
+		new_tab_section.node_name = child.name
+		new_tab_section.tab_name = child.tab_name
+		new_tab_section.tab_image = child.thumbnail 
+		
+		tab_section.add_child(new_tab_section)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -17,4 +35,8 @@ func _phone_back_button_pressed():
 
 # Opens tabs
 func _on_tabs_button_pressed():
-	pass # Replace with function body.
+	tab_section_panel.show()
+
+
+func _on_tab_exit_pressed():
+	tab_section_panel.hide()
---
+++ scenes/device/browser/tab_thumbnail.gd
@@ -0,0 +1,23 @@
+extends Control
+
+signal thumbnail_touched(tab_name: String)
+
+@onready var tab_thumb_name = $TabName
+@onready var tab_thumb_image = $TabThumbnail
+@export var node_name: String
+@export var tab_name: String
+@export var tab_image: String
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tab_thumb_name.text = tab_name
+	tab_thumb_image.texture = load(tab_image)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		thumbnail_touched.emit(tab_name)
