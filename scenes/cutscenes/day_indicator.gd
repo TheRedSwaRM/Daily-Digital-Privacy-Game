8f1822e66ff05f839d61d0fab65aade5df2ad4bf
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-02-18 20:25:18 +0800

    feat: skips day indicator if ever debug build

--- scenes/cutscenes/day_indicator.gd
+++ scenes/cutscenes/day_indicator.gd
@@ -4,14 +4,15 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	day_label.text = "Day " + str(Events.day_counter)
-	await get_tree().create_timer(3).timeout
-	
-	day_label.show()
-	await get_tree().create_timer(5).timeout
-	
-	day_label.hide()
-	await get_tree().create_timer(1).timeout
+	if not OS.is_debug_build():
+		day_label.text = "Day " + str(Events.day_counter)
+		await get_tree().create_timer(3).timeout
+		
+		day_label.show()
+		await get_tree().create_timer(5).timeout
+		
+		day_label.hide()
+		await get_tree().create_timer(1).timeout
 	
 	Events.change_map.emit("res://scenes/intro.tscn", false)
 	
