5905eaa1e44fe68adeed056d2ccee53b61c01548
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-02-20 01:21:11 +0800

    feat: allow time to pass

--- scenes/screen_tint.gd
+++ scenes/screen_tint.gd
@@ -5,7 +5,11 @@ extends CanvasModulate
 const min_per_hour = 60
 const game_to_irl_min = (2 * PI) / 1440
 
-@export var initial_hour: int
+@export_category("Time")
+@export var time_passes_allowed: bool = true
+@export var initial_hour: int = 6
+@export var game_speed: int = 20.0
+
 
 func _change_color(curr_time: float):
 	var new_time = game_to_irl_min * min_per_hour * curr_time
@@ -32,4 +36,6 @@ func _ready():
 func _process(delta):
 	if Engine.is_editor_hint():
 		_change_color(initial_hour)
+	else:
+		if time_passes_allowed: Events.game_time += delta * game_to_irl_min * game_speed
 	
