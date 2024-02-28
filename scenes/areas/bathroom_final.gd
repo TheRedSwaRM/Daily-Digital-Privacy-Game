66075b4b4e8315b6722cd84f136516bbf1f8331a
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-02-29 00:28:02 +0800

    feat: bathroom final version added

--- scenes/areas/bathroom_final.gd
+++ scenes/areas/bathroom_final.gd
@@ -12,4 +12,20 @@ func _process(delta):
 
 
 func _on_to_front_input_detected():
-	pass # Replace with function body.
+	AudioManager.sfx_play("res://assets/audio/sfx/door_open.mp3")
+
+
+func _on_shower_input_detected():
+	DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/bathroom.dialogue"), "shower")
+
+
+func _on_mirror_input_detected():
+	DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/bathroom.dialogue"), "mirror")
+
+
+func _on_faucet_input_detected():
+	DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/bathroom.dialogue"), "faucet")
+
+
+func _on_toilet_input_detected():
+	DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/bathroom.dialogue"), "toilet")
