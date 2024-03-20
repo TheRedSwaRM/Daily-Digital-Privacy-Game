5e6c5ed78374636f2210f2f58ebc1ae6cd2589a1
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-03-21 06:05:14 +0800

    feat: added scare cutscene

---
+++ scenes/cutscenes/hacked_scare_scene.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var anim_player = $AnimationPlayer
+@onready var scare_container = $ScareContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim_player.play("you_are_my_special")
+	await anim_player.animation_finished
+	scare_container.hide()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
