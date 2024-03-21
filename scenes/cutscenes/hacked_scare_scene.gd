32514305cb1600c996054a36c19a1fe6efe920b0
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-03-21 20:38:07 +0800

    feat: game ending logic complete

--- scenes/cutscenes/hacked_scare_scene.gd
+++ scenes/cutscenes/hacked_scare_scene.gd
@@ -8,6 +8,8 @@ func _ready():
 	anim_player.play("you_are_my_special")
 	await anim_player.animation_finished
 	scare_container.hide()
+	await get_tree().create_timer(2).timeout 
+	Events.quit_game()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/game_end.gd
+++ scenes/game_end.gd
@@ -1,9 +1,26 @@
 extends Control
 
+@onready var animation_player = $AnimationPlayer
+@onready var hacked_text = $HackedText
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var output = []
+	match OS.get_name():
+		"Windows":
+			OS.execute("CMD.exe", ["/C", "echo %USERNAME%"], output)
+		"macOS":
+			OS.execute("id", ["-un"], output)
+		"Linux", "FreeBSD", "NetBSD", "OpenBSD", "BSD":
+			OS.execute("id", ["-un"], output)
+		"Android":
+			output.append(OS.get_environment("USER"))
+		"iOS":
+			output.append(OS.get_environment("USER"))
+		
+	hacked_text.text = "..." + str(output[0])
+	
 	_check_for_route()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -11,6 +28,39 @@ func _process(_delta):
 
 # Checks on what to do when given something.
 func _check_for_route():
-	await get_tree().create_timer(5).timeout 
-	
-	Events.quit_game()
+	## If player is hacked.
+	if Events.hack_checker():
+		animation_player.play("hacked_ending")
+		await animation_player.animation_finished
+		await get_tree().create_timer(2).timeout
+		get_tree().change_scene_to_file("res://scenes/cutscenes/hacked_scare_scene.tscn")
+		
+	## If player can sleep.
+	elif Events.check_game_switch("PLAYER_can_sleep"):
+		animation_player.play("sleep_ending")
+		await animation_player.animation_finished
+		await get_tree().create_timer(2).timeout 
+		Events.quit_game()
+		
+	## If player spams until player blocks.
+	elif Events.check_game_switch("BLOCK_attacker_num"):
+		animation_player.play("block_ending")
+		await animation_player.animation_finished
+		await get_tree().create_timer(2).timeout 
+		Events.quit_game()
+		
+	## If player gets jebaited.
+	elif Events.check_game_switch("PLAYER_bamboozle"):
+		animation_player.play("baited_ending")
+		await animation_player.animation_finished
+		await get_tree().create_timer(2).timeout 
+		Events.quit_game()
+		
+	## Failsafe.
+	else:
+		#animation_player.play("hacked_ending")
+		#await animation_player.animation_finished
+		#await get_tree().create_timer(2).timeout
+		#get_tree().change_scene_to_file("res://scenes/cutscenes/hacked_scare_scene.tscn")
+		await get_tree().create_timer(5).timeout 
+		Events.quit_game()
