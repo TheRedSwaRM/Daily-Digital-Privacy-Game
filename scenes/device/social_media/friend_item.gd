54774a0fffc4c1b9af349dec5d5e73c0cfd17b08
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-03-27 03:35:26 +0800

    fix: changed friend item styling

--- scenes/device/social_media/friend_item.gd
+++ scenes/device/social_media/friend_item.gd
@@ -4,10 +4,11 @@ extends PanelContainer
 @onready var friend_text_line = $FriendItems/FriendsText
 
 @export var friend_name: String
-
+@export var friend_texture: Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if friend_texture != null: friend_icon_texture = friend_texture
 	friend_text_line.text = friend_name
 	name = friend_name
 	
