311dff6c0dee2c828904c7d2ced2018c47704691
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-03-26 18:44:08 +0800

    feat: added credits animation

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -1,8 +1,12 @@
 extends Control
 
+@onready var anim_player = $AnimationPlayer
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(5).timeout 
+	anim_player.play("credits")
+	await anim_player.animation_finished
 	Events.quit_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
