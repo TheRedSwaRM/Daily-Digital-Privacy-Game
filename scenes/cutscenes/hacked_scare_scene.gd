e9215082900891d99212a94aaa15fa512cbcb260
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-03-26 21:50:28 +0800

    feat: hacked scare scene now redirects to end of game

--- scenes/cutscenes/hacked_scare_scene.gd
+++ scenes/cutscenes/hacked_scare_scene.gd
@@ -9,7 +9,7 @@ func _ready():
 	await anim_player.animation_finished
 	scare_container.hide()
 	await get_tree().create_timer(2).timeout 
-	Events.quit_game()
+	Events.change_map.emit("res://scenes/game_end.tscn", false)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
