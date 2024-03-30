e795d6002bc9aafc1ce20f34c3ef050c6e25c9ff
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-03-30 14:17:06 +0800

    fix: commented possible errant code that might've been glitching the phone out in the third day

--- scenes/areas/bedroom_front.gd
+++ scenes/areas/bedroom_front.gd
@@ -21,21 +21,21 @@ func _on_to_bathroom_input_detected():
 
 
 func _on_click_area_component_input_detected():
-	if Events.check_game_switch("night"):
-		AudioManager.bgm_stop()
-		Events.deactivate_phone.emit()
-		
-		await get_tree().create_timer(1).timeout
-		
-		DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/knocking.dialogue"), "evening")
-		await DialogueManager.dialogue_ended
-
-		await get_tree().create_timer(1).timeout
-		
-		
-		Events.change_map.emit("res://scenes/cutscenes/creep_incoming.tscn")
-	else:
-		AudioManager.sfx_play("res://assets/audio/sfx/door_open.mp3")
-		Events.change_map.emit("res://scenes/areas/hallway.tscn")
-		#DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/knocking.dialogue"), "morning")
+	#if Events.check_game_switch("night"):
+		#AudioManager.bgm_stop()
+		#Events.deactivate_phone.emit()
+		#
+		#await get_tree().create_timer(1).timeout
+		#
+		#DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/knocking.dialogue"), "evening")
+		#await DialogueManager.dialogue_ended
+#
+		#await get_tree().create_timer(1).timeout
+		#
+		#
+		#Events.change_map.emit("res://scenes/cutscenes/creep_incoming.tscn")
+	#else:
+	AudioManager.sfx_play("res://assets/audio/sfx/door_open.mp3")
+	Events.change_map.emit("res://scenes/areas/hallway.tscn")
+	#DialogueManager.show_dialogue_balloon(load("res://assets/dialogue/knocking.dialogue"), "morning")
 		
