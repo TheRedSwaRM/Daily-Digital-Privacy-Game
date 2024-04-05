328991fe294beb19fb931fbbce57399c406391b8
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-04-06 00:37:00 +0800

    feat: added post survey stuff after finishing game 2

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -7,7 +7,7 @@ extends Control
 func _ready():
 	anim_player.play("credits")
 	await anim_player.animation_finished
-	OS.shell_open(" https://docs.google.com/forms/d/e/1FAIpQLSdzb_dsxALg1zrbO6vgG26UghK1bZ8sJdabM3-brL7Ledt_GA/viewform")
+	OS.shell_open("https://docs.google.com/forms/d/e/1FAIpQLSdzb_dsxALg1zrbO6vgG26UghK1bZ8sJdabM3-brL7Ledt_GA/viewform")
 	Events.quit_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
